4b3d2c2c77fbcef6aa8400bdb77e5770fd785439
brain <lussier.guillaume@gmail.com>
2017-05-26 14:56:24 +0200

    added non recursive version

--- java/BinarySearch.java
+++ java/BinarySearch.java
@@ -46,15 +46,37 @@ public class BinarySearch {
 	return index;
     }
 
+    /* non recursive implementation, still returns -1 if not found
+     */
+    public int loopSearch (int tofind, int[] inArray) {
+	int index = -1;
+
+	int start = 0;
+	int end = inArray.length -1;
+	while ((index == -1) && (start <= end)) {
+	    int mid = (start+end)/2;
+	    if (inArray[mid] == tofind)
+		index = mid;
+	    else if (inArray[mid] > tofind)
+		end = mid - 1;
+	    else
+		start = mid + 1;
+	}
+	return index;
+    }
+   
 
     public static void main (String[] args) {
-	BinarySearch thisprog = new BinarySearch(true);
+	BinarySearch thisprog = new BinarySearch(false);
 	int[] inputArray = {2, 4, 12, 54, 23, 8, 49};
 	int[] sortedArray = inputArray.clone();
 	Arrays.sort(sortedArray);
 	System.out.println("unsorted array: "+thisprog.toText(inputArray));
 	System.out.println("sorted array: "+thisprog.toText(sortedArray));
 	int index;
+
+	// testing with middle (no recursion) first, last and random position
+	System.out.println("Recursive Binary Search");
 	index = thisprog.arraySearch(12, sortedArray, 0, sortedArray.length);
 	System.out.println("Searching 12 at position 3 - result is "+index);
 	index = thisprog.arraySearch(2, sortedArray, 0, sortedArray.length);
@@ -62,6 +84,16 @@ public class BinarySearch {
 	index = thisprog.arraySearch(54, sortedArray, 0, sortedArray.length);
 	System.out.println("Searching 54 at position 6 - result is "+index);
 	index = thisprog.arraySearch(8, sortedArray, 0, sortedArray.length);
+	System.out.println("Searching 8 at position 2 - result is "+index);
+	
+	System.out.println("Non-Recursive Binary Search");
+	index = thisprog.loopSearch(12, sortedArray);
+	System.out.println("Searching 12 at position 3 - result is "+index);
+	index = thisprog.loopSearch(2, sortedArray);
+	System.out.println("Searching 2 at position 0 - result is "+index);
+	index = thisprog.loopSearch(54, sortedArray);
+	System.out.println("Searching 54 at position 6 - result is "+index);
+	index = thisprog.loopSearch(8, sortedArray);
 	System.out.println("Searching 8 at position 2 - result is "+index);
     }
 }
